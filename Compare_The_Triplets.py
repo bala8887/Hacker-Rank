d88f4826d30ea654489f833581e64f20c2b1aecc
bala8887 <45397262+bala8887@users.noreply.github.com>
2018-12-15 17:54:01 -0500

    Update Compare_The_Triplets.py

--- Compare_The_Triplets.py
+++ Compare_The_Triplets.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import math
 import os
 import random
@@ -6,6 +8,15 @@ import sys
 
 # Complete the compareTriplets function below.
 def compareTriplets(a, b):
+    a_val, b_val = 0,0;
+    for i in range(3):
+        if a[i]>b[i]:
+            a_val+=1;
+        if a[i]<b[i]:
+            b_val+=1;
+    
+    return [a_val,b_val];
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -20,3 +31,4 @@ if __name__ == '__main__':
     fptr.write('\n')
 
     fptr.close()
+
