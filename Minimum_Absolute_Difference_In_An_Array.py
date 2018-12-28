ba196f816f10696588a0ceb4fe2e5670a9a5de56
bala8887 <45397262+bala8887@users.noreply.github.com>
2018-12-27 19:08:35 -0500

    Create Minimum_Absolute_Difference_In_An_Array.py
    Easy 15

---
+++ Minimum_Absolute_Difference_In_An_Array.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr.sort();
+    lst=[];
+    for i in range(1,len(arr)):
+        lst.append(abs(arr[i-1]-arr[i]));
+    return min(lst);
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
