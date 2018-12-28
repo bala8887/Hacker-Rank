e2820f378f5842a4272e6dace5a12e86cf7a4d93
bala8887 <45397262+bala8887@users.noreply.github.com>
2018-12-27 19:09:33 -0500

    Create Marc's_Cakewalk.py
    Easy 15

---
+++ Marc's_Cakewalk.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the marcsCakewalk function below.
+def marcsCakewalk(calorie):
+    calorie.sort();
+    calorie.reverse();
+    total=0;
+    j=0;
+    for i in calorie:
+        total+=(pow(2,j)*i);
+        j=j+1;
+    return total;
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
