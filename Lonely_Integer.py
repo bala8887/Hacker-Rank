d5b0b610446dbdb8b9628595958cdf41ce87dda0
bala8887 <45397262+bala8887@users.noreply.github.com>
2018-12-27 19:27:42 -0500

    Create Lonely_Integer.py
    Easy 20

---
+++ Lonely_Integer.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from functools import reduce
+# Complete the lonelyinteger function below.
+def lonelyinteger(a):
+    return reduce((lambda x,y:x^y),a);
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
